2ee6336b773f4050ab551bca813debcafb596cc6
Fikry Fahrezy <55774934+fikryfahrezy@users.noreply.github.com>
2025-11-23 07:06:09 +0700

    chore(front-end): refactor file dropzone component

---
+++ front-end/src/components/file-dropzone/index.tsx
@@ -0,0 +1,94 @@
+import { ChangeEvent, useRef, useState } from "react";
+import styles from "./styles.module.css";
+import { noop } from "@/utils/noop";
+
+export type FileDropzoneProps = Omit<
+  React.ComponentProps<"input">,
+  "file" | "onChange"
+> & {
+  files?: FileList;
+  onChange?: (files: FileList) => void;
+};
+
+export function FileDropzone({
+  className,
+  children,
+  onChange = noop,
+  ...restProps
+}: FileDropzoneProps) {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDragging(false);
+  };
+
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDragging(false);
+
+    const files = event.dataTransfer?.files;
+    if (!files) {
+      return;
+    }
+
+    onChange(files);
+  };
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (!files) {
+      return;
+    }
+
+    onChange(files);
+  };
+
+  const onZoneClicked = () => {
+    fileInputRef.current?.click();
+  };
+
+  const onZoneKeydown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === "Enter" || event.key === " ") {
+      event.preventDefault();
+      fileInputRef.current?.click();
+    }
+  };
+
+  const style: React.CSSProperties = {
+    borderColor: isDragging ? "darkgreen" : "#ccc",
+    backgroundColor: isDragging ? "#f0fff0" : "#fff",
+  };
+
+  return (
+    <div
+      className={`${className} ${styles.zone}`}
+      style={style}
+      tabIndex={0}
+      role="button"
+      aria-label={restProps["aria-label"]}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+      onClick={onZoneClicked}
+      onKeyDown={onZoneKeydown}
+    >
+      <input
+        {...restProps}
+        ref={fileInputRef}
+        type="file"
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+        aria-label="File Dropzone"
+      />
+      {children}
+      {isDragging && <p style={{ color: "darkgreen" }}>Drop now!</p>}
+    </div>
+  );
+}
